578108262edb0ce2bd564782495aeaa0f22811d5
Kokonutuw <maxime.fortune08@gmail.com>
2026-05-20 14:54:02 +0200

    fix(feur): punctuation removed

--- bot.js
+++ bot.js
@@ -1,7 +1,6 @@
 import "dotenv/config";
-import sonnex from 'talisman/phonetics/french/sonnex.js';
+import sonnex from "talisman/phonetics/french/sonnex.js";
 import { Client, GatewayIntentBits, REST, Routes } from "discord.js";
-
 
 const client = new Client({
   intents: [
@@ -10,13 +9,14 @@ const client = new Client({
     GatewayIntentBits.MessageContent,
   ],
 });
+
 client.once("clientReady", async () => {
   console.log(`Logged in as ${client.user.tag}!`);
 
   const commands = [
     {
       name: "ping",
-      description: "get the bot's latency ",
+      description: "get the bot's latency",
     },
   ];
 
@@ -36,7 +36,6 @@ client.once("clientReady", async () => {
 });
 
 // Slash commands handler
-
 client.on("interactionCreate", async (interaction) => {
   if (!interaction.isChatInputCommand()) return;
 
@@ -52,22 +51,43 @@ client.on("interactionCreate", async (interaction) => {
 });
 
 // Detect "quoi / pourquoi / variants"
-const QUOI_PHONETIC = ["koi","kua", "kUa", "kva", "koa", "kuUa", "kuva", "kuua", "kUua", "kuoa", "kUva", "kUUa", "kUoa", "kUa", "kUaa", "keva"]
+const QUOI_PHONETIC = [
+  "koi",
+  "kua",
+  "kUa",
+  "kva",
+  "koa",
+  "kuUa",
+  "kuva",
+  "kuua",
+  "kUua",
+  "kuoa",
+  "kUva",
+  "kUUa",
+  "kUoa",
+  "kUa",
+  "kUaa",
+  "keva",
+];
 
 client.on("messageCreate", async (message) => {
   if (message.author.bot) return;
 
-  const msg = message.content.toLowerCase();
+  const match = message.content
+    .toLowerCase()
+    .normalize("NFD")
+    .replace(/[\u0300-\u036f]/g, "")
+    .match(/(\p{L}+)[^\p{L}]*$/u);
 
-  const words = msg.match(/[a-zàâçéèêëîïôûùüÿñæœ]+/gi);
-  console.log(words)
-  if (!words) return;
+  if (!match) return;
 
-  const lastWord = words[words.length - 1].replace(/(.)\1+/g, '$1').normalize("NFD").replace(/[\u0300-\u036f]/g, "");
-  console.log("last word:",lastWord);
+  const lastWord = match[1].replace(/(.)\1+/g, "$1");
+
+  console.log("last word:", lastWord);
 
   const lastWordPhonetic = sonnex(lastWord);
-  console.log("phonetic : ", lastWordPhonetic, "base comparison: ", QUOI_PHONETIC);
+
+  console.log("phonetic:", lastWordPhonetic, "base comparison:", QUOI_PHONETIC);
 
   if (QUOI_PHONETIC.includes(lastWordPhonetic)) {
     await message.reply("feur");
